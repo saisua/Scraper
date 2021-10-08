ea47b816004b681d5ad6f275c6a7c712d00ccd20
saisua <ausias.prieto@gmail.com>
2021-10-08 10:29:13 -0400

    Delete Tests directory

--- Tests/crawling_basic.py
+++
@@ -1,5 +0,0 @@
-from ..Core.Crawler import Crawler
-
-def test_crawler_basic(start_url:str = "https://duckduckgo.com/?t=ffab&q=hi&atb=v1-1&ia=web"):
-    with Crawler() as cr:
-        cr(start_url)
